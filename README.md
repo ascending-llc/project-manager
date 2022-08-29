d48a657d156eec201daf4018774907119a5727ec
Nathan Foster <nathancfoster.03@gmail.com>
2022-08-29 16:35:36 -0400

    Final changes and publish to project manager

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # AWS Project Manager
 
-Project manager used for AWS services, which contains frameworks used by ascending
+Project manager enabling users to keep data clean and organized, ensuring only one directory for code is needed.
+
+## Usage
+
+### **Installation**
+
+```bash
+npm i awspm -g
+```
+
+### or in newer npm versions
+
+```bash
+npm i awspm --location=global
+```
+
+### **Configuration**
+
+> Getting a GitHub token
+
+Go to [this website](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) and follow the steps to create a token. Make sure:
+- Your token has Full access to repos
+- Your token has Admin access to repo hooks
+- You have copied the token
+
+> CLI
+
+Run below command and follow the steps:
+
+```bash
+pm config
+```
+
+\* Please understand if you choose to be in an org any new project will be under that org.
+
+### **Adding Templates**
+
+Run below command and follow the steps: 
+
+```bash
+pm add
+```
+
+\* For Repo generation templates (aka Code templates) you will need access to the template, as well as the owner and the template name.
+
+### **Using the CLI**
+
+Run below command to either start a new repo or change repos to an existing one.
+
+```bash
+pm i
+```
+
+\* Running below command will delete all files in current directory 
--- commands/index.js
+++ commands/index.js
@@ -6,6 +6,10 @@ import templates from "../config/templates.json" assert { type: "json" };
 import axios from "axios";
 
 const add = () => {
+    if (!fs.existsSync(homedir() + "/.awspm")) {
+        shell.echo("Please run pm config to set up configuration files");
+        shell.exit();
+    }
     let data = {
         type: "",
         link: "",
@@ -28,7 +32,7 @@ const add = () => {
         },
         {
             type: "input",
-            message: "What is the link for this template (if code, please input the template generation link)",
+            message: "What is the git clone link for this template",
             name: "link",
             when: (e) => e.type === "Cloudformation template"
         },
@@ -45,15 +49,16 @@ const add = () => {
             when: (e) => e.type === "Code"
         }
     ]).then(element => {
+        const { type, name } = element
         fs.readFile(homedir() + "/.awspm/templates.json", (e, d) => {
             if (e) {
                 console.log(e)
             } else {
                 let data = JSON.parse(d);
-                let link = e.type === "Code" ? `https://api.github.com/repos/${e.owner}/${e.repo}/generate` : e.link
+                let link = type === "Code" ? `https://api.github.com/repos/${element.owner}/${element.repo}/generate` : element.link
                 let new_data = {
-                    type: e.type,
-                    name: e.name,
+                    type: type === "Code" ? "code" : "cloud" ,
+                    name,
                     link
                 }
                 data["templates"].push(new_data);
@@ -102,131 +107,135 @@ const config = () => {
 }
 
 const init = async () => {
-    if (!fs.existsSync(homedir() + "/.awspm")) {
-        shell.echo("Please run pm config to set up configuration files");
-        shell.exit();
-    }
-    let data = fs.readFileSync(homedir() + "/.awspm/templates.json");
-    let templates = JSON.parse(data);
-    let cloud_templates = templates["templates"].filter(e => e.type === "cloud");
-    let code_templates = templates["templates"].filter(e => e.type === "code");
-    let userdata = fs.readFileSync(homedir() + "/.awspm/userconfig.json");
-    let userconfig = JSON.parse(userdata);
-
-    let repo_link = `https://api.github.com/${userconfig.is_org === "Yes" ? `org/${userconfig.org}` : "user"}/repos`
-    let res = await axios.get(repo_link, {
-        headers: {
-            Accept: "application/vnd.github+json",
-            Authorization: "token " + userconfig.password
-        },
-        params: {
-            sort: "pushed"
+    try {
+        if (!fs.existsSync(homedir() + "/.awspm")) {
+            shell.echo("Please run pm config to set up configuration files");
+            shell.exit();
         }
-    })
-
-    let names = res.data.map(e => e.name);
-    shell.exec("find . -delete")
-    inquirer.prompt([
-        {
-            type: "list",
-            choices: [
-                "New",
-                "Existing"
-            ],
-            name: "choice1",
-            message: "Is this project a new or existing project"
-        },
-        {
-            type: "checkbox",
-            choices: [
-                "None",
-                ...cloud_templates.map(e => e.name)
-            ],
-            message: "Would you like any Cloud templates?",
-            name: "cloud_templates",
-            when: (e) => e.choice1 === "New"
-        },
-        {
-            type: "list",
-            choices: [
-                "Empty",
-                ...code_templates.map(e => e.name)
-            ],
-            message: "What code template would you like?",
-            name: "code_template",
-            when: (e) => e.choice1 === "New"
-        },
-        {
-            type: "list",
-            choices: [...names],
-            message: "Please choose which repo you're working on...",
-            name: "repo",
-            when: (e) => e.choice1 === "Existing"
-        },
-        {
-            type: "input",
-            message: "What is the name of the new Project?",
-            name: "name",
-            when: (e) => e.choice1 === "New"
-        },
-        {
-            type: "list",
-            message: "Is the project private or public?",
-            choices: [
-                "Private",
-                "Public"
-            ],
-            name: "is_private",
-            when: (e) => e.choice1 === "New"
-        },
-        {
-            type: "input",
-            message: "Basic description of the new app",
-            name: "desc",
-            when: (e) => e.choice1 === "New"
-        }
-    ]).then(async e => {
-        if (e.choice1 === "New") {
-            let post;
-            let data = {
-                "name": e.name,
-                "description": e.desc,
-                "private": e.is_private === "Private"
-            };
-            if (e.code_template === "Empty") {
-                post = userconfig.is_org === "Yes" ? "https://api.github.com/orgs/" + userconfig.org + "/repos" : "https://api.github.com/user/repos";
+        let data = fs.readFileSync(homedir() + "/.awspm/templates.json");
+        let templates = JSON.parse(data);
+        let cloud_templates = templates["templates"].filter(e => e.type === "cloud");
+        let code_templates = templates["templates"].filter(e => e.type === "code");
+        let userdata = fs.readFileSync(homedir() + "/.awspm/userconfig.json");
+        let userconfig = JSON.parse(userdata);
+    
+        let repo_link = `https://api.github.com/${userconfig.is_org === "Yes" ? `orgs/${userconfig.org}` : "user"}/repos`
+        let res = await axios.get(repo_link, {
+            headers: {
+                Accept: "application/vnd.github+json",
+                Authorization: "token " + userconfig.password
+            },
+            params: {
+                sort: "pushed"
+            }
+        })
+    
+        let names = res.data.map(e => e.name);
+        shell.exec("find . -delete")
+        inquirer.prompt([
+            {
+                type: "list",
+                choices: [
+                    "New",
+                    "Existing"
+                ],
+                name: "choice1",
+                message: "Is this project a new or existing project"
+            },
+            {
+                type: "checkbox",
+                choices: [
+                    "None",
+                    ...cloud_templates.map(e => e.name)
+                ],
+                message: "Would you like any Cloud templates?",
+                name: "cloud_templates",
+                when: (e) => e.choice1 === "New"
+            },
+            {
+                type: "list",
+                choices: [
+                    "Empty",
+                    ...code_templates.map(e => e.name)
+                ],
+                message: "What code template would you like?",
+                name: "code_template",
+                when: (e) => e.choice1 === "New"
+            },
+            {
+                type: "list",
+                choices: [...names],
+                message: "Please choose which repo you're working on...",
+                name: "repo",
+                when: (e) => e.choice1 === "Existing"
+            },
+            {
+                type: "input",
+                message: "What is the name of the new Project?",
+                name: "name",
+                when: (e) => e.choice1 === "New"
+            },
+            {
+                type: "list",
+                message: "Is the project private or public?",
+                choices: [
+                    "Private",
+                    "Public"
+                ],
+                name: "is_private",
+                when: (e) => e.choice1 === "New"
+            },
+            {
+                type: "input",
+                message: "Basic description of the new app",
+                name: "desc",
+                when: (e) => e.choice1 === "New"
+            },
+        ]).then(async e => {
+            if (e.choice1 === "New") {
+                let post = code_templates.find(d => d.name === e.code_template).link;
+                let data = {
+                    "name": e.name,
+                    "description": e.desc,
+                    "private": e.is_private === "Private"
+                };
+                if (e.code_template === "Empty") {
+                    post = userconfig.is_org === "Yes" ? "https://api.github.com/orgs/" + userconfig.org + "/repos" : "https://api.github.com/user/repos";
+                } else {
+                    if (userconfig.is_org === "Yes") data["owner"] = userconfig.org;
+                }
+                let res = await axios.post(post, data, {
+                    headers: {
+                        Accept: "application/vnd.github+json",
+                        Authorization: "token " + userconfig.password
+                    }
+                })
+                let repo_url = res.data.clone_url;
+    
+                shell.echo("Creating and cloning repo...")
+    
+                shell.exec("sleep 3")
+    
+                shell.exec(`git clone ${repo_url} . -q`)
+    
+                if (e.cloud_templates[0] !== "None") {
+                    for (const template in e.cloud_templates) {
+                        let template_name = e.cloud_templates[template];
+                        let cur_template = cloud_templates.find(e => e.name === template_name);
+                        
+                        shell.echo(`Cloning ${template}`);
+    
+                        shell.exec(`git clone ${cur_template.link} . -s`)
+                    }
+                }
             } else {
-                if (userconfig.is_org === "Yes") data["owner"] = userconfig.org;
-                post = e.code_template.link
+                let clone_url = res.data.filter(repo => repo.name === e.repo)[0].clone_url;
+                shell.exec(`git clone ${clone_url} . -q`);
             }
-            let res = await axios.post(post, data, {
-                headers: {
-                    Accept: "application/vnd.github+json",
-                    Authorization: "token " + userconfig.password
-                }
-            })
-            let repo_url = res.data.clone_url;
-
-            shell.echo("Creating and cloning repo...")
-
-            shell.exec("sleep 3")
-
-            shell.exec(`git clone ${repo_url} -s`)
-
-            if (e.cloud_templates[0] !== "None") {
-                for (const template in e.cloud_templates) {
-                    let cur_template = cloud_templates.filter(e => e.name === template);
-                    
-                    shell.echo(`Cloning ${template}`);
-
-                    shell.exec(`git clone ${cur_template.link} -s`)
-                }
-            }
-        } else {
-            let clone_url = res.data.filter(repo => repo.name === e.repo)[0].clone_url;
-            shell.exec(`git clone ${clone_url}`);
-        }
-    })
+        })       
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export default {
