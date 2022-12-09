1ff89eb845d5e2487b4a7d5013811cb3c24272f6
NathanCFoster <nathancfoster.03@gmail.com>
2022-12-09 12:52:01 -0500

    possible bug fix for dos to unix systems; wsl fix

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node --no-warnings
+#! /usr/bin/env -S node --no-warnings
 import { Command } from "commander";
 import json from "../package.json" assert { type: "json" };
 import commands from "../commands/index.js";
