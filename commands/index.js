8f0ce61b14e95712f23217f6a83137e4be3826a5
Nathan Foster <nathancfoster.03@gmail.com>
2023-01-09 16:00:54 -0500

    update 1.2.6

--- commands/index.js
+++ commands/index.js
@@ -442,7 +442,7 @@ const init = async (opts) => {
             shell.exit();
         }
         shell.echo(`Deleting old files`)
-        shell.exec("find . -delete")
+        // shell.exec("find . -delete")
 
         let data = fs.readFileSync(homedir() + "/.awspm/templates.json");
         let templates = JSON.parse(data);
@@ -476,11 +476,8 @@ const init = async (opts) => {
             },
             {
                 type: "checkbox",
-                choices: [
-                    "None",
-                    ...cloud_templates.map(e => e.name)
-                ],
-                message: "Would you like any Git boilerplate?",
+                choices: [...cloud_templates.map(e => e.name)],
+                message: "Select any Git Boilerplate you would like?",
                 name: "cloud_templates",
                 when: (e) => e.choice1 === "New"
             },
@@ -547,9 +544,9 @@ const init = async (opts) => {
     
                     shell.echo("Creating and cloning repo...")
     
-                    shell.exec("sleep 3")
+                    // shell.exec("sleep 3")
     
-                    shell.exec(`git clone ${repo_url} . -q`)
+                    // shell.exec(`git clone ${repo_url} . -q`)
     
                     if (e.cloud_templates[0] !== "None") {
                         for (const template in e.cloud_templates) {
@@ -558,7 +555,7 @@ const init = async (opts) => {
     
                             shell.echo(`Cloning ${template}`);
     
-                            shell.exec(`git clone ${cur_template.link} . -s`)
+                            // shell.exec(`git clone ${cur_template.link} . -s`)
                         }
                     }   
                 } catch (error) {
